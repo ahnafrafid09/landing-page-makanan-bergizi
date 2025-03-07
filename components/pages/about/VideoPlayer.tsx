ee5c0ce98c35cce83c3af4e9a077a49c3858eef7
Ahnaf <Ahnafrafid22@gmail.com>
2025-03-07 20:06:45 +0700

    add video

--- components/pages/about/VideoPlayer.tsx
+++ components/pages/about/VideoPlayer.tsx
@@ -1,22 +1,44 @@
-import Image from "next/image";
-import { Play } from "lucide-react";
+"use client";
+import { useRef, useState } from "react";
+import { Play, Pause } from "lucide-react";
 
 const VideoPlayer = () => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const togglePlayPause = () => {
+    if (videoRef.current) {
+      if (isPlaying) {
+        videoRef.current.pause();
+      } else {
+        videoRef.current.play();
+      }
+      setIsPlaying(!isPlaying);
+    }
+  };
+
   return (
     <div className="container mx-auto mt-24">
       <div className="relative w-full">
-        <Image
-          src="/video.png"
-          alt="Fake Video"
-          width={1000}
-          height={1000}
-          objectFit="cover"
-          className="w-full rounded-lg"
-        />
+        <video
+          ref={videoRef}
+          src="/assets/video.mp4"
+          className="w-full h-[700px] rounded-lg"
+          controls={false}
+          loop
+        ></video>
 
-        <button className="absolute inset-0 flex items-center justify-center">
+        {/* Tombol Play/Pause */}
+        <button
+          className="absolute inset-0 flex items-center justify-center"
+          onClick={togglePlayPause}
+        >
           <div className="w-16 h-16 bg-white rounded-full flex items-center justify-center shadow-lg">
-            <Play className="w-10 h-10 text-darken fill-darken" />
+            {isPlaying ? (
+              <Pause className="w-10 h-10 fill-darken text-darken cursor-pointer" />
+            ) : (
+              <Play className="w-10 h-10 fill-darken text-darken cursor-pointer" />
+            )}
           </div>
         </button>
       </div>
